1839c491dcf6a538d6ec7521b6b0898743c79fcb
Kamil Pietrzkiewicz <kamil.pietrzkiewicz@alterpage.pl>
2020-06-06 05:45:33 +0200

    zadanie 2

---
+++ zadanie_2/zadanie2.X/build/default/production/main2.p1.d
@@ -0,0 +1,2 @@
+build/default/production/main2.p1:  \
+main2.c 
---
+++ zadanie_2/zadanie2.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=zadanie2.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/zadanie2.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=zadanie2.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/zadanie2.x.tar
